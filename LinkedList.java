e867030e3880c3cce8dbfda70c4170398efdbc8b
Dolly Singh <dollyrajpoot07@gmail.com>
2022-08-06 21:33:17 +0530

    Initial Commit

---
+++ LinkedList.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class LinkedList {
+    Node head;
+    private int size;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    //add 1st
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    //add last
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while(currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void printList() {
+        if(head == null) {
+            System.out.println("Empty List");
+            return;
+        }
+        Node currNode = head;
+        while(currNode.next != null) {
+            System.out.println(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("Null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addFirst("D");
+        list.addFirst("OLL");
+        list.addLast("y");
+        list.printList();
+        list.addLast("u");
+
+    }
+}
